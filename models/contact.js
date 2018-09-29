7cbad185a4a97e1055dc523b77c1d74495791aa0
Roger <rdpender3@gmail.com>
2018-09-29 15:14:52 -0700

    setting up database and schema

---
+++ models/contact.js
@@ -0,0 +1,26 @@
+module.exports = function (sequelize, DataTypes) {
+  let Contact = sequelize.define("Contact", {
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [1]
+      }
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [1]
+      }
+    },
+    question: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+      validate: {
+        len: [1]
+      }
+    }
+  });
+  return Contact;
+};
