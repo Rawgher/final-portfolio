f70a57d137497b7cf9bbb5dd8a4523241bf0b109
Roger <rdpender3@gmail.com>
2018-10-02 22:37:56 -0700

    switching from handlebars

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,51 +1,24 @@
 var db = require("../models");
 
+var path = require("path");
+
 module.exports = function (app) {
-  // Load index page
-  app.get("/", function (req, res) {
-    res.render("index", {
-      title: "Home",
-      msg: "Welcome to my website!"
-    });
+
+  app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/index.html"));
   });
 
-  // Load the about me page
-  app.get("/AboutMe", function (req, res) {
-      res.render("about", {
-        title: "About Me",
-        msg: "About Me"
+  app.post("/", function (req, res) {
+      db.Contact.create({
+        name: req.body.name,
+        email: req.body.email,
+        subject: req.body.subject,
+        question: req.body.textInput
+      }).then(function () {
+        res.sendFile(path.join(__dirname, "../public/index.html"));
       });
     });
 
-  // Load the portfolio page
-  app.get("/Portfolio", function (req, res) {
-      res.render("portfolio", {
-        title: "Portfolio",
-        msg: "My Portfolio"
-      });
-    });
-
-  // Load the contact page
-  app.get("/Contact", function (req, res) {
-      res.render("contact", {
-        title: "Contact Me",
-        msg: "Contact Me"
-      });
-    });
-  
-  // Post contact requests to the database
-  app.post("/Contact", function (req, res) {
-    db.Contact.create({
-      name: req.body.name,
-      email: req.body.email,
-      subject: req.body.subject,
-      question: req.body.textInput
-    }).then(function () {
-      res.redirect("/Contact")
-    });
-  });
-
-  // Render 404 page for any unmatched routes
   app.get("*", function (req, res) {
     res.render("404");
   });
