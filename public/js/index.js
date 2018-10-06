df373bf4db4a964a1ade7e1c93e1869aa64f447a
Roger <rdpender3@gmail.com>
2018-10-05 21:41:31 -0700

    changed and adding style

--- public/js/index.js
+++ public/js/index.js
@@ -2,31 +2,31 @@ $(document).ready(function () {
     $('.tooltipped').tooltip();
 
     $("#homeButton").on("click", function() {
-        $("#aboutMe").addClass("hidden");
-        $("#portfolio").addClass("hidden");
-        $("#contactMe").addClass("hidden");
-        $("#home").removeClass("hidden");
+        $("#aboutMe").fadeOut(1).addClass("hidden");
+        $("#portfolio").fadeOut(1).addClass("hidden");
+        $("#contactMe").fadeOut(1).addClass("hidden");
+        $("#home").fadeIn("slow").removeClass("hidden");
     })
 
     $("#aboutButton").on("click", function() {
-        $("#home").addClass("hidden");
-        $("#portfolio").addClass("hidden");
-        $("#contactMe").addClass("hidden");
-        $("#aboutMe").removeClass("hidden");
+        $("#home").fadeOut(1).addClass("hidden");
+        $("#portfolio").fadeOut(1).addClass("hidden");
+        $("#contactMe").fadeOut(1).addClass("hidden");
+        $("#aboutMe").fadeIn("slow").removeClass("hidden");
     })
 
     $("#portButton").on("click", function() {
-        $("#home").addClass("hidden");
-        $("#aboutMe").addClass("hidden");
-        $("#contactMe").addClass("hidden");
-        $("#portfolio").removeClass("hidden");
+        $("#home").fadeOut(1).addClass("hidden");
+        $("#aboutMe").fadeOut(1).addClass("hidden");
+        $("#contactMe").fadeOut(1).addClass("hidden");
+        $("#portfolio").fadeIn("slow").removeClass("hidden");
     })
 
     $("#contactButton").on("click", function() {
-        $("#home").addClass("hidden");
-        $("#aboutMe").addClass("hidden");
-        $("#portfolio").addClass("hidden");
-        $("#contactMe").removeClass("hidden");
+        $("#home").fadeOut(1).addClass("hidden");
+        $("#aboutMe").fadeOut(1).addClass("hidden");
+        $("#portfolio").fadeOut(1).addClass("hidden");
+        $("#contactMe").fadeIn("slow").removeClass("hidden");
     })
 
 
