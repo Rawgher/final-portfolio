da14d8a03755e16932896b38a40facf37705cdaf
Roger <rdpender3@gmail.com>
2018-10-06 10:09:03 -0700

    changed some coloring

--- public/js/index.js
+++ public/js/index.js
@@ -2,34 +2,24 @@ $(document).ready(function () {
     $('.tooltipped').tooltip();
 
     $("#homeButton").on("click", function() {
-        $("#aboutMe").fadeOut(1).addClass("hidden");
         $("#portfolio").fadeOut(1).addClass("hidden");
         $("#contactMe").fadeOut(1).addClass("hidden");
         $("#home").fadeIn("slow").removeClass("hidden");
     })
 
-    $("#aboutButton").on("click", function() {
-        $("#home").fadeOut(1).addClass("hidden");
-        $("#portfolio").fadeOut(1).addClass("hidden");
-        $("#contactMe").fadeOut(1).addClass("hidden");
-        $("#aboutMe").fadeIn("slow").removeClass("hidden");
-    })
-
     $("#portButton").on("click", function() {
         $("#home").fadeOut(1).addClass("hidden");
-        $("#aboutMe").fadeOut(1).addClass("hidden");
         $("#contactMe").fadeOut(1).addClass("hidden");
         $("#portfolio").fadeIn("slow").removeClass("hidden");
     })
 
     $("#contactButton").on("click", function() {
         $("#home").fadeOut(1).addClass("hidden");
-        $("#aboutMe").fadeOut(1).addClass("hidden");
         $("#portfolio").fadeOut(1).addClass("hidden");
         $("#contactMe").fadeIn("slow").removeClass("hidden");
     })
 
     $('.modal').modal();
     $('.modal').open()
-    
+
 });
